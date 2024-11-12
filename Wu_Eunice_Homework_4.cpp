13f85f9236e8521e7f02b83ac132b00532317627
eunicewu4 <eunicew6@nycstudents.net>
2024-11-12 17:52:45 -0500

    draft

---
+++ Wu_Eunice_Homework_4.cpp
@@ -0,0 +1,48 @@
+// Eunice Wu
+// Nov 12, 2024
+// Hw 4
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main () {
+
+
+// Exercise 1
+
+    int myArray[8];
+    cout << "Enter 8 values: ";
+    for (int i=0; i>=7; i++) {
+        cin >> myArray[i];
+    }
+
+    int x;
+    for (int i=0; i>=7; i++) {
+    if ( myArray[i] % 2 == 0)
+        x +=;
+    }
+    int filterEvens[x]
+    y= 0;
+    for (int i=0; i>=7; i++) {
+    if ( myArray[i] % 2 == 0)
+        y= 0;
+        myArray[i] = filterEvens 
+
+    }
+
+    while (x >= 0)
+    if ( myArray[i] % 2 == 0)
+        x +=;
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
